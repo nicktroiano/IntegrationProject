781c469fe4c1bd3e7a7c833fa57eb7da59869589
nicktroiano <ndtroiano7093@eagle.fgcu.edu>
2019-09-17 13:29:53 -0400

    first commit

--- Main.py
+++ Main.py
@@ -1,4 +1,14 @@
 # Nick Troiano
 # A Trivia Game
 
-print("Welcome to my trivia game!") #pythontrivia?
+print("Welcome to my trivia game!")
+
+input("Would you like to play baseball trivia?")
+if input == "yes":
+ print("Good Luck")
+if input == "no":
+        exit(0)
+input("Who won the World Series in 2018?")
+if input == "Chicago Cubs" or "Cubs" or "chicago cubs" or "cubs":
+        print("Congratulations, next question.")
+else: "Better luck next time, try again."
